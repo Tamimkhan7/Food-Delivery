da6b06433da6587343519b9009de19ac158a8e7c
Tamimkhan7 <mdtamimkhan842@gmail.com>
2025-06-23 15:17:25 +0600

    added appdownload and loginpopup and update some others file

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,62 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import {assets} from '../../assets/assets'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
 
-    //usestate use kori basically data store korar jonno button click korle value change korte and UI real time update korte 
-    const [menu, setMenu] = useState("home");
-    
   return (
-    <div className='navbar'>
-      <img src= {assets.logo} alt="" className="logo"/>
-          <ul className="navbar-menu">
-              <li onClick={() => setMenu("home")} className= {menu==="home"? "active" : ""}>Home</li>
-              <li onClick={() => setMenu("menu")} className= {menu==="menu"? "active" : ""}>Menu</li>
-              <li onClick={() => setMenu("mobile-app")} className= {menu==="mobile-app"? "active" : ""}>Mobile-App</li>
-              <li onClick={() => setMenu("contact-us")} className= {menu==="contact-us"? "active" : ""}>Contact Us</li>
-          </ul>
-          <div className="navbar-right">
-              <img src={assets.search_icon} alt="" />
-              <div className="navbar-search-icon">
-                  <img src={assets.basket_icon} alt="" />
-                  <div className="dot"></div>
-              </div>
-              <button>Sing In</button>
-          </div>
+    <div className="navbar">
+      <img src={assets.logo} alt="Logo" className="logo" />
+      <ul className="navbar-menu">
+        <li>
+          <Link
+            to="/"
+            onClick={() => setMenu("home")}
+            className={menu === "home" ? "active" : ""}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <a
+            href="#explore-menu"
+            onClick={() => setMenu("menu")}
+            className={menu === "menu" ? "active" : ""}
+          >
+            Menu
+          </a>
+        </li>
+        <li>
+          <a
+            href="#app-download"
+            onClick={() => setMenu("mobile-app")}
+            className={menu === "mobile-app" ? "active" : ""}
+          >
+            Mobile-App
+          </a>
+        </li>
+        <li>
+          <a
+            href="#footer"
+            onClick={() => setMenu("contact-us")}
+            className={menu === "contact-us" ? "active" : ""}
+          >
+            Contact Us
+          </a>
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="Basket" />
+          <div className="dot"></div>
+        </div>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
